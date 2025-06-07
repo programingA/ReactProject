24a4258da699a92477323a0f2ceeaf424243a66c
Chen <fnel2003@naver.com>
2025-06-08 02:13:01 +0900

    missCodeUpdate

--- src/component/FindPassword.jsx
+++ src/component/FindPassword.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function() {
     return(
         <>
-            <Link to='/LoginPage'>홈으로 가기</Link>
+            <Link to='/'>홈으로 가기</Link>
             <h1>구현되지 않은 페이지입니다.</h1>
         </>
     )
